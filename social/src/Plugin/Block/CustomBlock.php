b998ef5a8eebe6c607d35ced6a3a5b120c546da4
Artyom <hovasapyan_artyom03@mail.ru>
2022-01-28 16:43:24 +0400

    update

--- social/src/Plugin/Block/CustomBlock.php
+++ social/src/Plugin/Block/CustomBlock.php
@@ -45,3 +45,68 @@ class CustomBlock extends BlockBase {
   }
 
 }
+
+
+// kamel
+
+/*
+
+<?php
+
+namespace Drupal\social\Plugin\Block;
+
+use Drupal\Core\Url;
+use Drupal\Core\Link;
+use Drupal\Core\Block\BlockBase;
+
+/**
+
+ *
+ * @Block(
+ *   id = "custom_block",
+ *   admin_label = @Translation("Custom block"),
+ *   category = @Translation("Custom Block"),
+ * )
+ */
+class CustomBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $list = array();
+    function add_social_links($url,$link_options,$text,&$list){
+      $url = Url::fromUri($url);
+      $url->setOptions($link_options);
+      $link = Link::fromTextAndUrl($text, $url);
+      $list[] = $link;
+    }
+    add_social_links("https://facebook.com",[
+      'attributes' => [
+        'target' => 'fb_blank',
+        'title' => 'Link to Facebook page',
+      ],
+    ],"Facebook",$list);
+
+    add_social_links("https://instagram.com",[
+      'attributes' => [
+        'target' => 'instagram_blank',
+        'title' => 'Link to Instagram page',
+      ],
+    ],"Instagram",$list);
+    add_social_links("https://vk.com",[],"VK",$list);
+
+    $output['my_links_page'] = [
+      '#theme' => 'item_list',
+      '#items' => $list,
+      '#title' => $this->t('Social Media Links'),
+    ];
+
+    return $output;
+
+  }
+
+}
+*/
+
+
